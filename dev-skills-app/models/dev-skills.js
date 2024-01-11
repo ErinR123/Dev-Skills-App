c3a932c8a532ec763fb07d2f8eef70c8468417cf
Erin Robinson <r.erin2546@gmail.com>
2024-01-11 20:57:50 +1030

    fourth commit- links added

--- dev-skills-app/models/dev-skills.js
+++ dev-skills-app/models/dev-skills.js
@@ -1,9 +1,8 @@
 const devSkills = [
-    { id: 0, devskill: "Javascript", done: true },
-    { id: 1, devskill: "DOM", done: true },
-    { id: 2, devskill: "Python", done: false },
+    { id: 0, devskill: "Javascript", done: true, doclink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+    { id: 1, devskill: "DOM", done: true, doclink: "https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model" },
+    { id: 2, devskill: "Python", done: false, doclink: "https://developer.mozilla.org/en-US/docs/Glossary/Python" },
   ];
-  
   function getAll() {
     return devSkills;
   }
